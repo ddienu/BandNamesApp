6bdbefd135e0258bfc030c54d8093e2c4935b29b
ddienu <rockdann@hotmail.com>
2022-11-25 00:31:30 -0500

    Update: home page.
    Update home page turning from a stateless to a stefulwidget, in the state create a List of type band to use the model created in the last update, with 5 length, with its id, name and votes, also in the scaffold adding an appbar with a title and  a floating action button to give some visual enhaces to the UI, in the body return a listview builder with a ListTile to create a circle avatar with the first two letters of the band's name in the leading, in the title the band's name, the trailing the band's votes and the onTap method a print with the band's name to check that the tap is working

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,16 +1,70 @@
 import 'package:flutter/material.dart';
 
+import 'package:band_names_app/src/models/band.dart';
 
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  List<Band> bands = [
+    Band(
+      id: '1', name: 'Metallica', votes: 5
+      ),
+    Band(
+      id: '2', name: 'Aerosmith', votes: 3
+      ),
+    Band(
+      id: '3', name: 'Amon Amarth', votes: 2
+      ),
+    Band(
+      id: '4', name: 'Stratovarius', votes: 6
+      ),
+    Band(
+      id: '5', name: 'Megadeth', votes: 2
+      )
+      
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+      appBar: AppBar(
+        title: Text('BandNames', style: TextStyle( color: Colors.black),),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 1,
       ),
+      body: ListView.builder(
+        itemCount: bands.length,
+        itemBuilder: (BuildContext context, int index) => _bandTile(bands[index])
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon( Icons.add ),
+        elevation: 1,
+        backgroundColor: Colors.blue[400],
+        ),
+    );
+  }
+
+  ListTile _bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+            backgroundColor: Colors.blue[200],
+            child: Text( band.name.substring(0, 2).toUpperCase()),
+          ),
+      title: Text( band.name),
+      trailing: Text('${band.votes}', style: TextStyle( fontSize: 20),),
+      onTap: (){
+        print(band.name);
+      },
     );
   }
 }
